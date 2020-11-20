4b7ebcb7e9a0db6c249f6f312d984926d6e579c5
EudaldoAlvarez <71477511+EudaldoAlvarez@users.noreply.github.com>
2020-11-19 19:36:48 -0700

    primer avance

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Shared film</title>
+	<link rel="stylesheet" type="text/css" href="assets/style/login.css? v=1">
+</head>
+<body>
+	<!--==================
+		Header-start
+	===================-->
+	<div id="header">
+		<div class="logo">
+			<a href="">
+				<img src="assets/img/header/logo.png">
+			</a>
+			
+		</div>
+		<div class="links">
+			<ul>
+				<li>
+					<a href="">Movies</a>
+				</li>
+				<li>
+					<a href="">Loans</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="assets/img/header/user.png">
+					</a>
+				</li>
+				
+			</ul>
+		</div>
+	</div>
+	<!--==================
+		Header-end
+	===================-->
+	<!--==================
+		login-start
+	===================-->
+	<div id="login">
+		<div class="container">
+			<div class="login-register">
+				<div class="sign-in">
+					<label>Sign In</label>
+					<a href="">Register</a>
+				</div>
+				<div class="formulario">
+					<form>
+						<label>
+							User
+						</label>
+						<input type="text" name="user" placeholder="User">
+						<label>
+							Password
+						</label>
+						<input type="password" name="password" placeholder="*****">
+						<button>
+							SING IN
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--==================
+		login-end
+	===================-->
+	<!--==================
+		footer-start
+	===================-->
+	<div id="footer">
+		<div class="info">
+			<div class="">
+				<label>
+					ABOUT THE COMPANY
+				</label>
+				<p>
+					Nosotros somos un grupo de estudiantes que realizaron esta pagina con la intencion de pasar la materia de progrmacion web
+				</p>
+			</div>
+			<div class="social-links">
+				<ul>
+					<li>
+						<a href="">
+							<img src="">
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="">
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="">
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="">
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="aboot us">
+			
+		</div>
+	</div>
+	<!--==================
+		footer-end
+	===================-->
+</body>
+</html>
