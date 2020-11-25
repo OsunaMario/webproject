31f7c61e4e2371fc1c42c9a1d903bf3ecdd3af5e
Mario Osuna <71477284+OsunaMario@users.noreply.github.com>
2020-11-25 04:34:46 -0700

    Segundo avance
    Home, estructura general, navbar, sidebar de categories y years

---
+++ home/index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Miles de Películas y series Online en calidad HD, Castellano y Subtitulado sin cortes. Somos DuodePelis Oficial, ver series y peliculas online gratis.">
+	<title>DuoPelis - Ver Películas Online Gratis</title>
+	<link rel="stylesheet" type="text/css" href="../assets/style/home.css">
+	<script src="https://kit.fontawesome.com/820f15aa26.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+	<div class="header">
+		<div class="header-logo">
+			<a href="" title="DuodePelis">
+				<img src="../assets/img/header/logo.png">
+			</a>
+		</div>
+		<div class="header-right pull-right">
+			<div class="header-menu pull-left">
+				<ul>
+					<li class="active">
+						<a title="Ir a inicio" href="./">
+							<i class="fas fa-home"></i>
+							Inicio
+						</a>
+					</li>
+					<li>
+						<a title="Ir a películas" href="./peliculas">
+							<i class="fas fa-star"></i>
+							Películas
+						</a>
+					</li>
+					<li>
+						<a title="Ir a categorías" href="./categorias">
+							<i class="fas fa-columns"></i>
+							Categorías
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div id="menu-user" class="header-user pull-right">
+				<img class="user_active" src="../assets/img/profile.png" height="32">
+				<i class="fas fa-chevron-down">
+				</i>
+				
+
+				<!-- Intentando hacer un dropdown con quick-info de cuenta
+
+				<div class="dropdown">
+					<p>
+						<b id="username"></b>
+					</p>
+					<div class="divider"></div>
+				</div>
+				<a href="./cuenta" class="btn">
+					Mi cuenta
+					<i class="fas fa-user"></i>
+				</a>
+				<a href="./" class="btn logout-user" id="logout">
+					Salir
+					<i class="fas fa-sign-out-alt"></i> -->
+
+				</a>
+			</div>
+			<div class="header-search pull-right">
+				<input type="search" id="searchtext" placeholder="Buscar película" onkeyup="window.globalSearch(this)" name="Searchbar">
+				<div class="icon text-white text-center">
+					<div class="c-search">
+						<i class="fas fa-search"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="main">
+		<div class="main-left">
+			<div class="genres">
+				<h2>Géneros de películas</h2>
+				<ul>
+
+					<li>
+						<a href="./genero/accion">
+							<i class="fas fa-cube"></i>
+							Acción
+							<span class="cant-genre pull-right">2020</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="./genero/animacion">
+							<i class="fas fa-cube"></i>
+							Animación
+							<span class="cant-genre pull-right">7542</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="./genero/anime">
+							<i class="fas fa-cube"></i>
+							Anime
+							<span class="cant-genre pull-right">4259</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="./genero/artesmarciales">
+							<i class="fas fa-cube"></i>
+							Artes marciales
+							<span class="cant-genre pull-right">1025</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="./genero/aventura">
+							<i class="fas fa-cube"></i>
+							Aventura
+							<span class="cant-genre pull-right">628</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="./genero/baile">
+							<i class="fas fa-cube"></i>
+							Baile
+							<span class="cant-genre pull-right">785</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="./genero/belico">
+							<i class="fas fa-cube"></i>
+							Bélico
+							<span class="cant-genre pull-right">1540</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="./genero/biografico">
+							<i class="fas fa-cube"></i>
+							Biográfico
+							<span class="cant-genre pull-right">2304</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="./genero/catastrofe">
+							<i class="fas fa-cube"></i>
+							Catástrofe
+							<span class="cant-genre pull-right">4021</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="./genero/cienciaficcion">
+							<i class="fas fa-cube"></i>
+							Ciencia ficción
+							<span class="cant-genre pull-right">2978</span>
+						</a>
+					</li>
+					<div class="genre-more pull-right">
+						<a href="#">Ver más</a>
+					</div>
+				</ul>
+			</div>
+			
+			<div class="years">
+						<h2>Año de lanzamiento</h2>
+						<ul>
+
+							<li>
+								<a href="./peliculas-2020">2020</a>
+							</li>
+
+							<li>
+								<a href="./peliculas-2019">2019</a>
+							</li>
+
+							<li>
+								<a href="./peliculas-2018">2018</a>
+							</li>
+
+							<li>
+								<a href="./peliculas-2017">2017</a>
+							</li>
+
+							<li>
+								<a href="./peliculas-2016">2016</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2015">2015</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2014">2014</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2013">2013</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2012">2012</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2011">2011</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2010">2010</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2009">2009</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2008">2008</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2007">2007</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2006">2006</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2005">2005</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2004">2004</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2003">2003</a>
+							</li>
+							
+							<li>
+								<a href="./peliculas-2002">2002</a>
+							</li>
+							
+						</ul>
+					</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
