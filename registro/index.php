af6ef9e317c8c7b962def73991c8411d05500a2d
EudaldoAlvarez <71477511+EudaldoAlvarez@users.noreply.github.com>
2020-11-27 02:25:19 -0700

    avance 3

--- registro/index.php
+++ registro/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Shared film</title>
-	<link rel="stylesheet" type="text/css" href="assets/style/login.css? v=6">
+	<link rel="stylesheet" type="text/css" href="../assets/style/registro.css? v=6">
 </head>
 <body style="margin:0;">
 	<div id="container">
@@ -13,14 +13,14 @@
 		<div id="header">
 			<div class="logo">
 				<a href="home/index.php">
-					<img src="assets/img/header/logo.png">
+					<img src="../assets/img/header/logo.png">
 				</a>
 				
 			</div>
 			<div class="links">
 				<ul>
 					<li>
-						<a href="home/index.php">
+						<a href="../home/index.php">
 							<img src="">
 							Inicio
 						</a>
@@ -44,8 +44,8 @@
 					</li>
 					<li>
 						<div class="user">
-							<a href="registro/index.php">
-								<img src="assets/img/header/user.png">
+							<a href="">
+								<img src="../assets/img/header/user.png">
 							</a>
 						</div>
 					</li>
@@ -64,26 +64,34 @@
 				<fieldset>
 					<legend>
 						<label>
-							Iniciar sesion
+							Registro
 						</label>
 					</legend>
-					<img src="assets/img/header/user.png">
+					<img src="../assets/img/header/user.png">
 					<form>
-					<label>
-						Usuario
-					</label>
-					<input type="text" name="user" placeholder="User" required="">
-					<label>
-						Contraseña
-					</label>
-					<input type="password" name="password" placeholder="*****" required="">
-					<button>
-						Iniciar sesion
-					</button>
-				</form>
-				<label>
-					¿No estas registrado? pulsa <a href="registro/index.php">aquí</a>
-				</label>
+						<label>
+							Usuario
+						</label>
+						<input type="text" name="user" placeholder="User" required="">
+						<label>
+							email
+						</label>
+						<input type="email" name="email" placeholder="email" required="">
+						<label>
+							Contraseña
+						</label>
+						<input type="password" name="password" placeholder="*****" required="">
+						<div class="condiciones">
+							<label>
+								Aceptar <a href="">terminos y condiciones</a>
+							</label>
+							<input type="checkbox" name="terminos" required="">
+						</div>
+						<button>
+							Registrar
+						</button>
+					</form>
+				
 				</fieldset>
 			</div>
 		</div>
@@ -106,22 +114,22 @@
 				<ul>
 					<li>
 						<a href="">
-							<img src="assets/img/footer/fb.png">
+							<img src="../assets/img/footer/fb.png">
 						</a>
 					</li>
 					<li>
 						<a href="">
-							<img src="assets/img/footer/tw.png">
+							<img src="../assets/img/footer/tw.png">
 						</a>
 					</li>
 					<li>
 						<a href="">
-							<img src="assets/img/footer/in.png">
+							<img src="../assets/img/footer/in.png">
 						</a>
 					</li>
 					<li>
 						<a href="">
-							<img src="assets/img/footer/g.png">
+							<img src="../assets/img/footer/g.png">
 						</a>
 					</li>
 				</ul>
